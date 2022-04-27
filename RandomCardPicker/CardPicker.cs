fac222a966502476f073e686b717b0e992a24739
IfunanyaWilliam <williamifunanya@gmail.com>
2022-04-27 12:36:07 +0100

    Implement RandomSuit() and RandomValue() methods

--- RandomCardPicker/CardPicker.cs
+++ RandomCardPicker/CardPicker.cs
@@ -8,6 +8,7 @@ namespace RandomCardPicker
 {
     public class CardPicker
     {
+        static Random random = new Random();
         public static string[] PickSomeCards(int numberOfCards)
         {
             string[] pickedCards = new string[numberOfCards];
@@ -16,6 +17,27 @@ namespace RandomCardPicker
                 pickedCards[i] = RandomValue() + " of " + RandomSuit();
             }
             return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            //get a random number from 1 to 4
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
         }
     }
 }
